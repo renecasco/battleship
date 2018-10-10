c830eaa72d7f1c2b32d17f39581ec3df18a429ee
Rene Casco <rene.casco@icloud.com>
2018-10-09 21:45:25 -0600

    Cell class and tests updated and completed

--- lib/cell.rb
+++ lib/cell.rb
@@ -11,11 +11,17 @@ class Cell
     @peg = nil
   end
 
-  def place_peg(peg)
-    @peg = peg
+# I think we're overcomplicating here, peg value should be the same as the output for get_row. That way we would save ourselves a few lines of code and processing time of 'if' statements in get_row if we use "W" and "R" or "H" or "M" instead of :miss and :hit. We would only call the value stored in peg. In that same line of thought, peg should be initialized with a space " ". We wouldn't need to make all those decsisions or conversions to output to screen on print_board / get_row. Let's keep it simple.
+
+  def place_peg
+    if @contents == nil
+      @peg = :miss
+    else
+      @peg = :hit
+    end
   end
 
-  def point_to_ship(ship)
+  def change_contents(ship)
     @contents = ship
   end
 end
--- test/cell_test.rb
+++ test/cell_test.rb
@@ -15,22 +15,31 @@ class CellTest < Minitest::Test
   def test_it_has_attributes
     cell = Cell.new("A1")
     assert_equal "A1", cell.name
-    assert_equal nil, cell.contents
-    assert_equal nil, cell.peg
+    assert_nil cell.contents
+    assert_nil cell.peg
   end
 
-  def test_it_places_peg
+  def test_it_changes_contents
     cell = Cell.new("A1")
-    actual = cell.place_peg(:hit)
+    ship = Ship.new("A1", "A2", "Aircraft Carrier")
+    actual = cell.change_contents(ship)
+    expected = ship
+    assert_equal expected, actual
+  end
+
+  def test_it_places_hit_peg
+    cell = Cell.new("A1")
+    ship = Ship.new("A1", "A2", "Aircraft Carrier")
+    cell.change_contents(ship)
+    actual = cell.place_peg
     expected = :hit
     assert_equal expected, actual
   end
 
-  def test_it_points_to_ship
+  def test_it_places_miss_peg
     cell = Cell.new("A1")
-    ship = Ship.new("A1", "A2", "Aircraft Carrier")
-    actual = cell.point_to_ship(ship)
-    expected = ship
+    actual = cell.place_peg
+    expected = :miss
     assert_equal expected, actual
   end
 
