8bb540312357561fea0cb3c792cdd175164ff774
Rene Casco <rene.casco@icloud.com>
2018-10-10 10:59:18 -0600

    Player class WIP

--- lib/ship.rb
+++ lib/ship.rb
@@ -3,7 +3,7 @@ class Ship
               :length,
               :hit_count
 
-  # I removed the starting cell and end cell because on Sal's line of thought the board (through it's cells already knows which cells contain the ship). Also we discussed  the possibility of giving the responsibility of placing ships to the board. So doing board.place_ship(start_cell, end_cell) makes more sense! Once a peg is placed on a cell that contains a ship we would call ship.hit! to increment ship.hit_count and then ship.sunk? that compares the hit_count to the length of the ship and returns a boolean wether it was sunk or not.
+  # I removed the starting cell and end cell because on Sal's line of thought the board (through it's cells already knows which cells contain the ship). Also we discussed  the possibility of giving the responsibility of placing ships to the board. So doing board.place_ship(start_cell, end_cell) makes more sense! Once a peg is placed on a cell that contains a ship we would call ship.hit! to increment ship.hit_count and then call ship.sunk? that compares the hit_count to the length of the ship and returns a boolean wether it was sunk or not.
 
 
   # Also we define the name and length of the ships, not the user, example given in the test.
--- test/player_test.rb
+++ test/player_test.rb
@@ -1,2 +1,23 @@
 require 'simplecov'
 SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+
+  def test_it_exists
+    board_1 = Board.new
+    player_1 = Player.new("Joe", board_1)
+    assert_instance_of Player, player_1
+  end
+
+  def test_it_has_attributes
+    board_1 = Board.new
+    player_1 = Player.new("Joe", board_1)
+    assert_equal board_1, player_1.board
+    assert_equal "Joe", player_1.name
+  end
+
+end
