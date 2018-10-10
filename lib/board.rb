9ede8d686645fcc3fb85c98c59e176a04254c860
William Petty <william@snakeweed.com>
2018-10-10 15:26:09 -0600

    Adds board refactoring and output test

--- lib/board.rb
+++ lib/board.rb
@@ -14,35 +14,34 @@ class Board
   end
 
   def print_board
-    grid_string = "========================\n"
-    grid_string += ". 1 2 3 4 5 6 7 8 9 10\n"
-    y = 0
-    while y <= 9 do
-      grid_string += get_row(y)
-
-      y += 1
-    end
-    grid_string += "========================"
+    print get_board_string
   end
 
-  def get_row(y)
-    #Place letter at front of row string
-    row_string = ("A".."J").to_a[y]
-    #Place visual cell graphics in row string
-    @cell_grid[y].each do |cell|
-      row_string += cell.cell_graphic
+  def get_board_string
+    board_string = border
+    board_string += header_row
+    y = 0
+    while y <= 9 do
+      board_string += board_row(y)
+      y += 1
+    end
+    board_string += border
+  end
+
+  def border
+    "=======================\n"
+  end
+
+  def header_row
+    ". 1 2 3 4 5 6 7 8 9 10\n"
+  end
+
+  def board_row(y)
+    row_string = ("A".."J").to_a[y] #Place letter at front of row string
+    @cell_grid[y].each do |cell| #Place visual cell graphics in row string
+      row_string += " " + cell.peg
     end
     row_string += "\n"
-    row_string
-
-  def cell_graphic(cell)
-    if cell.peg == :hit
-      " R"
-    elsif cell.peg == :miss
-      " W"
-    else
-      "  "
-    end
   end
 
 end
